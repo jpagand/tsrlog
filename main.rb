3846f7bb45ff9f7f86421a7922245eb40dedaa94
David Pedersen <david.pdrsn@gmail.com>
2017-08-24 10:00:39 +0200

    Initial commit

---
+++ main.rb
@@ -0,0 +1,46 @@
+require "thread"
+require "pp"
+require "active_support/all"
+
+def env
+  envs = [
+    "development",
+    "staging",
+    "production",
+  ]
+  unless envs.include?(ARGV.first)
+    STDERR.puts(<<-EOS)
+Usage: logit [#{envs.join('|')}]
+    EOS
+    exit 1
+  end
+  env = ARGV[0]
+end
+
+file_name = "logit #{env}.log"
+
+Thread.new do
+  `heroku logs -t -r #{env} >> "#{file_name}"; say fail`
+end
+
+seen_lines = {}
+
+loop do
+  print "> "
+  input = STDIN.gets.chomp
+
+  case input
+  when ""
+  when "exit"
+    exit 0
+  when "failed", "fail", "f"
+    log_lines = File.read(file_name).lines.map(&:chomp)
+
+    new_lines = log_lines.select { |line| seen_lines[line].nil? }
+    puts new_lines.grep(/completed [^2]/i)
+
+    log_lines.each { |log_line| seen_lines[log_line] = true }
+  else
+    puts `cat "#{file_name}" | ag "#{input}"`
+  end
+end
